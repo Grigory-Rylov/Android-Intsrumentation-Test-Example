1889a4191df4b4c74d331786babdf0d122f6ffb5
Grigory Rylov <grishberg@gmail.com>
2018-01-11 09:55:43 +0300

    emulator plugin ver 0.3.9

--- buildSrc/src/main/groovy/com/github/grishberg/instrumentaltestsample/RunTestPlugin.groovy
+++ buildSrc/src/main/groovy/com/github/grishberg/instrumentaltestsample/RunTestPlugin.groovy
@@ -1,9 +1,6 @@
 package com.github.grishberg.instrumentaltestsample
 
 import com.github.grishberg.androidemulatormanager.CreateAndRunEmulatorsTask
-import com.github.grishberg.androidemulatormanager.DisplayMode
-import com.github.grishberg.androidemulatormanager.EmulatorConfig
-import com.github.grishberg.androidemulatormanager.ext.EmulatorManagerConfig
 import com.github.grishberg.androidemulatormanager.StopEmulatorsTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -11,7 +8,6 @@ import org.gradle.api.Project
 class RunTestPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        EmulatorManagerConfig emulatorsConfig = project.extensions.findByType(EmulatorManagerConfig)
         CreateAndRunEmulatorsTask createAndRunEmulatorsTask = project.tasks.findByName(CreateAndRunEmulatorsTask.NAME)
         StopEmulatorsTask stopEmulatorsTask = project.tasks.findByName(StopEmulatorsTask.NAME)
         stopEmulatorsTask.mustRunAfter "connectedAndroidTest"
@@ -33,14 +29,6 @@ class RunTestPlugin implements Plugin<Project> {
             finalizedBy installApkTask
             finalizedBy 'assembleDebug'
             finalizedBy 'assembleAndroidTest'
-
-            doLast {
-                EmulatorConfig argPhone = new EmulatorConfig("test_phone", DisplayMode.PHONE_HDPI, 26)
-                argPhone.setDiskSize(2048)
-                EmulatorConfig[] configs = [argPhone]
-                emulatorsConfig.setEmulatorArgs(configs)
-                emulatorsConfig.setWaitingTimeout(60 * 3 * 1000)
-            }
         }
     }
 }
